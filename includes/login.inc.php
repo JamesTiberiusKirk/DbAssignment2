02a1d8a2d448ec4d026fa4faa677ece99bed7c20
darthvader <dumitru.v.dv@gmail.com>
2019-11-03 11:27:48 +0000

    Fixed bug in login, still doesn't work propperly tho

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -7,42 +7,39 @@ if(isset($_POST['login-submit'])){
 
   //input validation
   if(empty($uname) || empty ($upass)){
-    header("Location: ../../pages/public/login.php?error=emptyCreds");
+    header('Location: ../../pages/public/login.php?error=emptyCreds');
     exit();
   } else {
-    $sql = "SELECT * FROM users WHERE uname=? OR email=?";
+    $sql = 'SELECT * FROM users WHERE uname=? OR email=?';
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)){
-      header("Location: ../../pages/public/login.php?error=sqlError");
+      header('Location: ../../pages/public/login.php?error=sqlError');
       exit();
     } else {
-      mysqli_stmt_bind_param($stmt, "ss", $uname, $uname);
+      mysqli_stmt_bind_param($stmt, 'ss', $uname, $uname);
       mysqli_stmt_execute($stmt);
       $result = mysqli_stmt_get_result($stmt);
       $row = mysqli_fetch_assoc($result);
       if($row){
         $hashedpwd = $row['upass'];
-        $pwdCheck = password_verify($upass, $hashedpwd);
-        if (password_verify($upass, $hashedpwd)){
-          header("Location: ../../pages/public/login.php?error=WrongPass&pass=".(string)$pwdCheck);
+        if (!password_verify($upass, $hashedpwd)){
+          header('Location: ../pages/public/login.php?error=WrongPass');
           exit();
         } else {
           session_start();
           $_SESSION['uID'] = $row['uID'];
           $_SESSION['uname'] = $row['uname']; 
 
-          header("Location: ../../pages/public/login.php?login=success");
+          header('Location: ../index.php?login=success');
           exit();
         }
       } else {
-        header("Location: ../../pages/public/login.php??error=NoSuchUser");
+        header('Location: ../pages/public/login.php??error=NoSuchUser');
         exit();
       }
     }
   }
-
-  
 } else {
-  header("Location: ../../index.php");
+  header('Location: ../index.php');
   exit();
 }
