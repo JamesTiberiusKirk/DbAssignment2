287f4a8fe1362987cc4a372b178357195d01ae7f
darthvader <dumitru.v.dv@gmail.com>
2019-11-03 10:47:11 +0000

    Misc stuff

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     include_once("./includes/db.inc.php");
     echo "<br>Connected successfully<br>";
 
-    $sql = "SELECT uID, uname FROM testapp.users";
+    $sql = "SELECT uID, uname, upass FROM testapp.users";
 
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["uID"] . " - username: " . $row["uname"] . "<br>";
+            echo "id: " . $row["uID"] . " - username: " . $row["uname"];
+            echo " - password: ". $row["upass"] . "<br>";
         }
     } else {
         echo "0 results";
