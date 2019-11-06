9d67e40be93f1d2befba8360fd743e9ef0b2c2fa
darthvader <dumitru.v.dv@gmail.com>
2019-11-06 11:26:35 +0000

    Changed index to display login status

--- index.php
+++ index.php
@@ -2,25 +2,13 @@
 
 <div class="jumbotron">
     <?php
-    include_once("./includes/db.inc.php");
-    echo "<br>Connected successfully<br>";
 
-    $sql = "SELECT uID, uname, upass FROM testapp.users";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while ($row = $result->fetch_assoc()) {
-            echo "id: " . $row["uID"] . " - username: " . $row["uname"];
-            echo " - password: ". $row["upass"] . "<br>";
-        }
+    if(isset($_SESSION['uID'])){
+        echo "<br>Logged in<br>";
     } else {
-        echo "0 results";
+        echo "<br>Logged out<br>";
     }
-    
-    
-    $conn->close();
+
     ?>
 </div>
 
