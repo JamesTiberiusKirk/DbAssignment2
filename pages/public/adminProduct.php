1addfa704473970daf794fcbff6133f3c7d549c2
Aleaf <aallgaier@dundee.ac.uk>
2019-11-11 13:54:57 +0000

    Added a adminProduct view page, it is unfinished

---
+++ pages/public/adminProduct.php
@@ -0,0 +1,13 @@
+<?php include "../../includes/header.php" ?>
+
+<div class="jumbotron">
+    <!---<div class="d-flex justify-content-center">
+        <h1>About</h1>
+    </div> --->
+    <div class="row">
+        <form action="../../includes/adminProduct.inc.php" method="post">
+                
+        </form>
+    </div>
+    <div class="col"></div>
+</div>
