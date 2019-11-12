11ba5900baef9b7ade57095dc382b01ffc6042a2
Aleaf <aallgaier@dundee.ac.uk>
2019-11-12 14:00:50 +0000

    add button

--- pages/admin/productView.php
+++ pages/admin/productView.php
@@ -5,11 +5,13 @@
     <div class="col">
         <div class="container">
             <div class="row">
-            <form class="input-group mb-3" method="post">
-                <input class="form-control" name="table_inp" type="text" placeholder="Search Product name" method="post">
-                <button class="btn btn-outline-secondary" name="search_btn" type="submit">Search</button>
-                <button class="btn btn-outline-secondary" name="show_tbl">Show Table</button>
-            </form> <!--- I dont know how to do this part i will attempt it later --->
+                <h1>Admin Product View</h1>
+                <form class="input-group mb-3" method="post">
+                    <input class="form-control" name="table_inp" type="text" placeholder="Search Product name" method="post">
+                    <button class="btn btn-outline-secondary" name="search_btn" type="submit">Search</button>
+                    <!-- <button class="btn btn-outline-secondary" name="show_tbl">Show Table</button> -->
+                    <button type="button" class="btn btn-outline-secondary">Add</button>
+                </form> <!--- I dont know how to do this part i will attempt it later --->
                 <table class="table table-dark">
                     <thead>
                         <tr>
@@ -30,20 +32,19 @@
                             if($result->num_rows > 0){
                                 while($row = $result->fetch_assoc()){
                                     echo '<tr>';
-                                        echo '<th scope="row">' . $row["ProductID"] . '</th>';
-                                        echo '<td>' . $row["Name"] . '</td>';
-                                        echo '<td>' . $row["Type"] . '</td>';
-                                        echo '<td>' . $row["CurrentPrice"] . '</td>';
-                                        echo '<td> # </td>';
-                                        echo '<td> <button type="button" class="btn btn-secondary">edit</button> </td>';
+                                    echo '<th scope="row">' . $row["ProductID"] . '</th>';
+                                    echo '<td>' . $row["Name"] . '</td>';
+                                    echo '<td>' . $row["Type"] . '</td>';
+                                    echo '<td>' . $row["CurrentPrice"] . '£' . '</td>';
+                                    echo '<td> # </td>';
+                                    echo '<td> <button type="button" class="btn btn-secondary">edit</button> </td>';
                                     echo '</tr>';
                                 }
                             } else {
                                 echo '<tr>';
-                                        echo "Zero results";
+                                echo "Zero results";
                                 echo '</tr>';
                             }
-
                             $conn->close();
                         ?>
                     </tbody>
