9088a91db1b548f15c8b33a3921e173844b0b1ab
Youssless <yaseenlahmami98@gmail.com>
2019-11-13 10:55:30 +0000

    added staff pages

--- includes/header.php
+++ includes/header.php
@@ -72,14 +72,14 @@ session_start();
                         ?>
                     </li>    
                         <?php
+                        include $_SERVER['DOCUMENT_ROOT'].'/includes/db.inc.php';
                         if (isset($_SESSION['uID'])) {
-                            include_once($_SERVER['DOCUMENT_ROOT'].'/includes/db.inc.php');
-                            $role_sql = 'SELECT uID FROM users WHERE uID=? AND urole =?';
+                            $role_sql = 'SELECT uID FROM users WHERE uID=? AND urole ="admin"';
                             $stmt = mysqli_stmt_init($conn);
 
                             if (mysqli_stmt_prepare($stmt, $role_sql)) {
                             
-                                mysqli_stmt_bind_param($stmt, "ss", $_SESSION['uID']);
+                                mysqli_stmt_bind_param($stmt, "s", $_SESSION['uID']);
                                 mysqli_stmt_execute($stmt);
                                 mysqli_stmt_store_result($stmt);
                                 $role_res = mysqli_stmt_num_rows($stmt);
@@ -92,7 +92,21 @@ session_start();
                         }
 
                         if (isset($_SESSION['uID'])) {
-                            include once($_SERVER['DOCUMENT_ROOT'].'/includes/db.inc.php');
+                            $role_sql = 'SELECT uID FROM users WHERE uID=? AND urole ="staff"';
+                            $stmt = mysqli_stmt_init($conn);
+
+                            if (mysqli_stmt_prepare($stmt, $role_sql)) {
+                            
+                                mysqli_stmt_bind_param($stmt, "s", $_SESSION['uID']);
+                                mysqli_stmt_execute($stmt);
+                                mysqli_stmt_store_result($stmt);
+                                $role_res = mysqli_stmt_num_rows($stmt);
+                                if ($role_res == 1) {
+                                    echo '<li class="nav-item">';
+                                    echo '<a class="nav-link" href="/pages/staff/index.php">Staff</a>';
+                                    echo '</li>';
+                                } 
+                            }
 
                         }
                         ?>
--- mysql/EtworldExamples.sql
+++ mysql/EtworldExamples.sql
@@ -1,50 +1,114 @@
-DROP DATABASE IF EXISTS `ETWorld`;
-
-CREATE DATABASE `ETWorld`;
-
 USE `ETWorld`;
 
-# Dump of table SUPPLIER
-# ------------------------------------------------------------
-DROP TABLE IF EXISTS `Supplier`;
-
-CREATE TABLE `Supplier`(
-`SupplierID`int(5) NOT NULL AUTO_INCREMENT,
-`SupplierAddress` varchar(30) DEFAULT NULL,
-`UnitPrice` float(25,5) DEFAULT NULL,
-PRIMARY KEY(`SupplierID`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
+LOCK TABLES `Supplier` WRITE;
 
-DROP TABLE IF EXISTS `Account`; 
+INSERT INTO `Supplier`(`SupplierID`,`SupplierAddress`,`UnitPrice`)
+VALUES
+(1765,'DundeeStock',5784.36),
+(63231,'DundeeStock',78451.5),
+(73375,'DundeeStock',4451.3);
+/*!40000 ALTER TABLE `Supplier` ENABLE KEYS */;
 
-CREATE TABLE `Account`(
-`AccountID` int(15) NOT NULL AUTO_INCREMENT,
-`AccountType` varchar(10) DEFAULT NULL, 
-`Username` varchar(15) DEFAULT NULL, 
-`Password` varchar(25) DEFAULT NULL, 
-PRIMARY KEY(`AccountID`)
+UNLOCK TABLES;
 
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-DROP TABLE IF EXISTS `Customer`;
-
-CREATE TABLE `Customer` (
-  `AccountID` int(15) DEFAULT NULL, 
-  `CustomerID` int(10) NOT NULL AUTO_INCREMENT,
-  `CustomerFirstName` varchar(10) DEFAULT NULL,
-  `CustomerLastName` varchar(10) DEFAULT NULL,
-  `CustomerAddress` varchar(50) DEFAULT NULL,
-  `Phone` varchar(12) DEFAULT NULL,
-  PRIMARY KEY (`CustomerID`),
-  KEY `fk_Customer_Account` (`AccountID`),
-CONSTRAINT `fk_Customer_Account` FOREIGN KEY (`AccountID`) REFERENCES `Account` (`AccountID`) ON DELETE SET NULL ON UPDATE CASCADE
-  
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+# Dump of table Customer
+# -------------------------------------SUPPLIER-----------------------
 
 
+LOCK TABLES `Customer` WRITE;
+/*!40000 ALTER TABLE `Customer` DISABLE KEYS */;
+
+INSERT INTO `Customer` (`AccountID`,`CustomerFirstName`,`CustomerLastName`,`CustomerAddress`,`Phone`)
+VALUES
+    ("",'Bellic','Niko','LIBERTYCITY',07579000000),
+    ("",'Bellic','Roman','LIBERTYCITY',07579000001),
+    ("",'Little','Jacob','LIBERTYCITY',07579000002),
+    ("",'Mcreary','Francis','LIBERTYCITY',07579000003),
+    ("",'Mcreary','Patrick','LIBERTYCITY',07579000004);
+
+/*!40000 ALTER TABLE `Customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+
+LOCK TABLES `CustomerOrder` WRITE;
+/*!40000 ALTER TABLE `CustomerOrder` DISABLE KEYS */;
+INSERT INTO `CustomerOrder` (`Quantity`,`OrderPrice`,`Delivery Address`,`Time`,`CustomerID`)
+VALUES
+	(1,8000,'LIBERTYCITY','2019-10-10',""),
+	(3,3300,'LIBERTYCITY','2019-10-11',""),
+	(2,4900,'LIBERTYCITY','2019-10-12',""),
+	(1,1500,'LIBERTYCITY','2019-10-13',""),
+  (1,1000,'LIBERTYCITY','2019-10-14',"");
+   
+
+/*!40000 ALTER TABLE `CustomerOrder` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `Branch` WRITE;
+/*!40000 ALTER TABLE `Branch` DISABLE KEYS */;
+INSERT INTO `Branch` (`SupplierID`,`BranchID`,`BranchType`,`BranchAddress`,`ContactNumber`,`StockQuantity`)
+VALUES
+	(12874,'DundeeBranch','Accountancy','Dundee DD1 5EN','764516317','9'),
+    (16574,'DundeeBranch','SELLsDept','Dundee DD1 5EN','75787656317','4');
+/*!40000 ALTER TABLE `Branch` ENABLE KEYS */;
+
+UNLOCK TABLES;
+
+LOCK TABLES `Account` WRITE;
+/*!40000 ALTER TABLE `Account` DISABLE KEYS */;
+INSERT INTO `Account`(`CustomerID`,`AccountID`,`AccountType`,`Username`,`Password`)  
+VALUES
+(0000000001,45574,'Buyer','JOhny1','something'),
+(0000000002,45312,'Supplier','BASELTD','something');
+/*!40000 ALTER TABLE `Account` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `BankAccount` WRITE;
+/*!40000 ALTER TABLE `BankAccount` DISABLE KEYS */;
+INSERT INTO `BankAccount` (`AccountID` , `BankAccountID`,`CardNUmber`,`CVC`,`AcountNUmber`,`SortCode`,`ExpiryDate`,`FullName`,`CardType`)
+VALUES
+
+("","",8888557457,325,125478,215478,1025, "Johny Bravo","Visa"),
+("","",5666557457,325,125478,215478,1025, "Johny Mckenna ","Visa");
+/*!40000 ALTER TABLE `BankAccount` ENABLE KEYS */;
+UNLOCK TABLES;
+
+LOCK TABLES `Product` WRITE;
+/*!40000 ALTER TABLE `Product` DISABLE KEYS */;
+/*10000-Laptop 20000-DesktopPc 30000-GammingLp 40000-GamingDp*/
+INSERT INTO `Product`(`ProductID`,`Name`,`Type`,`CurrentPrice`)
+
+(45574,4646,8888557457,325,125478,215478,1025, "Johny Bravo","Visa"),
+(45312,4746,5666557457,325,125478,215478,1025, "Johny Mckenna ","Visa");
+/*!40000 ALTER TABLE `BankAccount` ENABLE KEYS */;
+
+/*!40000 ALTER TABLE `PRODUCT` DISABLE KEYS */;
+/*10000-Laptop 20000-DesktopPc 30000-GammingLp 40000-GamingDp*/
+UNLOCK TABLES;
+
+
+LOCK TABLES `Staff` WRITE; 
+/*!40000 ALTER TABLE `Staff` DISABLE KEYS */;
+INSERT INTO `Staff` (`BranchID`, `AccountID`, `StaffID`,`FullName`,`Salary`, `Role`,`Address`,`Phone`)
+VALUES 
+('DundeeBranch','45574',456,"",32223,"","",08854574),
+('DundeeBranch','45312',123,"",32223,"","",08854574);
+/*!40000 ALTER TABLE `Staff` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+LOCK TABLES `Payroll` WRITE;
+
+/*!40000 ALTER TABLE `Payroll` DISABLE KEYS */;
+INSERT INTO `Payroll`(`StaffID`,`FullName`,`PayrollID`,`Deductions`,`GrossPay`,`NetPay`,`Ni`)
+VALUES
+(456,"Simon STill",0,"-something","Some ammount wihtout taxes","After taxes", "SB6548746D");
+/*!40000 ALTER TABLE `Payroll` ENABLE KEYS */;
+UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `CustomerOrder`;
 
@@ -68,7 +132,7 @@ DROP TABLE IF EXISTS `Branch`;
 
 CREATE TABLE `Branch` (
   `SupplierID` int(5) DEFAULT NULL  ,
-  `BranchID` int(15) NOT NULL AUTO_INCREMENT  ,  
+  `BranchID` int(15) AUTO_INCREMENT NOT NULL ,  
   `BranchType` varchar(10) NOT NULL,
   `BranchAddress` varchar(30) DEFAULT NULL,
   `ContactNumber` varchar(11)DEFAULT NULL,
@@ -98,8 +162,6 @@ KEY `fk_Product_CustomerOrder` (`CustomerOrderID`),
 CONSTRAINT `fk_Product_CustomerOrder` FOREIGN KEY (`CustomerOrderID`) REFERENCES `CustomerOrder` (`CustomerOrderID`) ON DELETE SET NULL ON UPDATE CASCADE,
 PRIMARY KEY(`ProductID`)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
 
 
 DROP TABLE IF EXISTS `BankAccount`;
@@ -161,7 +223,3 @@ CONSTRAINT `fk_Payroll_Staff` FOREIGN KEY (`StaffID`) REFERENCES `Staff` (`Staff
 PRIMARY KEY(`PayrollID`)
 
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-
-
