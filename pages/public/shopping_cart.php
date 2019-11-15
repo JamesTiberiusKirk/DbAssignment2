b1c14e62e0dddb3c8831ebec5fc8566e6166b719
Aleaf <aallgaier@dundee.ac.uk>
2019-11-15 16:07:12 +0000

    Updates for database for product pages and created product page

--- pages/public/shopping_cart.php
+++ pages/public/shopping_cart.php
@@ -7,8 +7,8 @@
         <div class="card shopping-cart">
             <div class="card-header bg-dark text-light">
                 <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                Shipping cart
-                <a href="" class="btn btn-outline-info btn-sm pull-right">Continiu shopping</a>
+                Shopping cart
+                
                 <div class="clearfix"></div>
             </div>
             <div class="card-body">
@@ -25,7 +25,9 @@
                         </div>
                         <div class="col-12 col-sm-12 text-sm-center col-md-4 text-md-right row">
                             <div class="col-3 col-sm-3 col-md-6 text-md-right" style="padding-top: 5px">
-                                <h6><strong>25.00 <span class="text-muted">x</span></strong></h6>
+                            <div class="row">
+                                <h6><strong>25.00£<span class="text-muted">x</span></strong></h6>
+                            </div>
                             </div>
                             <div class="col-4 col-sm-4 col-md-4">
                                 <div class="quantity">
@@ -35,9 +37,9 @@
                                 </div>
                             </div>
                             <div class="col-2 col-sm-2 col-md-2 text-right">
-                                <button type="button" class="btn btn-outline-danger btn-xs">
-                                    <i class="fa fa-trash" aria-hidden="true"></i>
-                                </button>
+                                <!---<button type="button" class="btn btn-outline-danger btn-xs"> --->
+                                    <a class="btn btn-outline-danger btn-xs" onclick="return confirm('Are you sure you want to delete this item?');">Trash</a>
+                                <!--- </button> --->
                             </div>
                         </div>
                     </div>
@@ -53,7 +55,7 @@
                 <div class="pull-right" style="margin: 10px">
                     <a href="" class="btn btn-success pull-right">Checkout</a>
                     <div class="pull-right" style="margin: 5px">
-                        Total price: <b>50.00€</b>
+                        Total price: <b>50.00£</b>
                     </div>
                 </div>
             </div>
