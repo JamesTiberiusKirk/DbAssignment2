07b1b2cc4de370a7dc957d9328eaab618103b2a1
Youssless <yaseenlahmami98@gmail.com>
2019-11-17 17:42:37 +0000

    staff finished apart from payroll

--- pages/admin/staff_create.php
+++ pages/admin/staff_create.php
@@ -11,6 +11,8 @@
         <input class="form-control" id="aci" name="acc_id_inp" type="text">
         <label for="jti">Job Title</label>
         <input class="form-control" id="jti" name="job_title_inp" type="text">
+        <label for="si">Salary</label>
+        <input class="form-control" id="jti" name="salary_inp" type="text">
         <label for="bii">Branch ID</label>
         <select class="mdb-select md-form" name='branch_id_inp' id='bii'>
             <option value="" >Choose branch ID</option>
@@ -28,6 +30,7 @@
     </form>
 
     <?php
+    include_once $_SERVER['DOCUMENT_ROOT'].'/includes/query.inc.php';
     //mysqli_next_result($conn);
     if (isset($_POST['add_staff_btn'])) {
         if (!empty($_POST['full_name_inp']) && !empty($_POST['acc_id_inp']) 
@@ -40,28 +43,26 @@
                 while ($row = $result->fetch_assoc()) {
                     $acc_type = $row['AccountType'];
                 }
-    
-                if ($acc_type === 'staff') {
+                mysqli_free_result($result);
+                if ($acc_type === 'customer') {
+                    $sql = 'UPDATE Account SET AccountType="staff" WHERE AccountID="'.$_POST['acc_id_inp'].'"';
+                    $result = mysqli_query($conn, $sql);
                     mysqli_free_result($result);
-                    $sql = 'INSERT INTO Staff (FullName, AccountID, BranchID, Role)
-                                    VALUES (?, ?, ?, ?)';
-    
-                    $stmt = mysqli_stmt_init($conn);
-                    if (!mysqli_stmt_prepare($stmt, $sql)) {
-                        die("dberr:".mysqli_error($conn));
-                    }
-                    else {
-                        mysqli_stmt_bind_param($stmt, 'siis', $_POST['full_name_inp'], $_POST['acc_id_inp'],
-                            $_POST['branch_id_inp'], $_POST['job_title_inp']);
-                        mysqli_stmt_execute($stmt) or die("dberr:".mysqli_error($conn));
-                        mysqli_stmt_store_result($stmt);
-                        mysqli_stmt_get_result($stmt);
-                        mysqli_stmt_free_result($stmt);    
-                        mysqli_stmt_close($stmt);
-                    
-    
+                    create_staff($conn);
+                    //create_payroll($conn);
+                    header('Location: /pages/admin/staff_manager.php?success');
+                }
+                else if ($acc_type === 'staff') {
+                    $sql = 'SELECT * FROM Staff WHERE AccountID="'.$_POST['acc_id_inp'].'"';
+                    $result = mysqli_query($conn, $sql);
+                    if ($result->num_rows == 0) {
+                        mysqli_free_result($result);
+                        create_staff($conn);
+                        //create_payroll($conn);
                         header('Location: /pages/admin/staff_manager.php?success');
                     }
+                    else
+                        echo "account is already a staff";
                 }
                 else {
                     echo "Account is not a staff";
@@ -76,6 +77,43 @@
     if(isset($_POST['cancel_btn'])) {
         header('Location: /pages/admin/staff_manager.php?canceled%staff%create');
     }
+
+    function create_staff($conn) {
+
+        $sql = 'INSERT INTO Staff (FullName, AccountID, BranchID, Role, Salary)
+        VALUES (?, ?, ?, ?, ?)';
+
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+        die("dberr:".mysqli_error($conn));
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, 'siisd', $_POST['full_name_inp'], $_POST['acc_id_inp'],
+            $_POST['branch_id_inp'], $_POST['job_title_inp'], $_POST['salary_inp']);
+            mysqli_stmt_execute($stmt) or die("dberr:".mysqli_error($conn));
+            mysqli_stmt_store_result($stmt);
+            mysqli_stmt_get_result($stmt);
+            mysqli_stmt_free_result($stmt);    
+            mysqli_stmt_close($stmt);
+        }
+    }
+
+    // function create_payroll($conn) {
+    //     $sql = 'SELECT FROM Staff WHERE AccountID = "'.$_POST['acc_id_inp'].'"';
+    //     $result = mysqli_query($conn, $sql);
+    //     $staff_id = 0;
+    //     if ($result->num_rows) {
+    //         while ($row = $result->fetch_assoc()) {
+    //             $staff_id = $row['staff_id'];
+    //         }
+    //     }
+    //     mysqli_free_result($result);
+    //     $stmt = mysqli_stmt_init($conn);
+    //     $sql = 'INSERT INTO Payroll (FullName, StaffID) VALUES (?, ?)';
+    //     $_stmt = bind_query($conn, $stmt, $sql, 'si', array($_POST['full_name_inp'], $staff_id));
+    //     mysqli_stmt_free_result($_stmt);
+    //     mysqli_stmt_close($_stmt);
+    // }
     ?>
 </div>
 
